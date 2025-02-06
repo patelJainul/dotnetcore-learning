5764e46e3fb1bf06d67b92c40870c26d6ca11c1f
Jainul Patel <jainul.h.patel@gmail.com>
2025-02-07 03:37:58 +0530

    Update target framework to net8.0 and enhance routing with new endpoint mappings in RoutingExamples

--- Routing/RoutingExamples/Program.cs
+++ Routing/RoutingExamples/Program.cs
@@ -1,6 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+app.UseEndpoints(endpoints => {
+    _ = endpoints.MapPost("static", async context => await context.Response.WriteAsync("test!"));
+    _ = endpoints.MapGet("file/{filename}.{ext:minlength(3)?}", async context => {
+        string? fileName = context.Request.RouteValues["filename"] as string;
+        string? ext = context.Request.RouteValues["ext"] as string;
+        await context.Response.WriteAsync($"Filename: {fileName}, Extension: {ext}");
+    });
+});
+
+app.Run(async (context) => await context.Response.WriteAsync("Hello World!"));
 
 app.Run();
