0e9ea1c3e82d1f3917d2efae9177b1e65278077b
Jainul Patel <jainul.h.patel@gmail.com>
2025-02-16 15:24:58 +0530

    Add layout and view files for weather application; update HomeController to use ViewBag for background colors

--- ViewAssignment/ViewAssignmentWeather/Controllers/HomeController.cs
+++ ViewAssignment/ViewAssignmentWeather/Controllers/HomeController.cs
@@ -1,32 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
+
 using ViewAssignmentWeather.Models;
 
 namespace ViewAssignmentWeather.Controllers
 {
     public class HomeController : Controller
     {
-        readonly List<CityWeather> cityWeathers = [
+        readonly List<CityWeather> _cityWeathers = [
             new CityWeather { CityUniqueCode = "LDN", CityName = "London", DateAndTime = DateTime.Now, TempretureFahrenheit = 33 },
             new CityWeather { CityUniqueCode = "NYC", CityName = "New York", DateAndTime = DateTime.Now, TempretureFahrenheit = 60 },
             new CityWeather { CityUniqueCode = "PAR", CityName = "Paris", DateAndTime = DateTime.Now, TempretureFahrenheit = 82 },
         ];
+
+        readonly Dictionary<string, string> _bgcolor = new Dictionary<string, string> {
+            { "blue", "bg-blue-300" },
+            { "green", "bg-[#d7ff79]" },
+            { "red", "bg-[#f5c448]" }
+        };
+
         // GET: HomeController
         [Route("/")]
         [Route("weather")]
         public ActionResult Index()
         {
-            return View(cityWeathers);
+            ViewBag.bgcolor = _bgcolor;
+            return View(_cityWeathers);
         }
 
         [Route("weather/{id}")]
         public ActionResult Weather(string id)
         {
-            var selectedCity = cityWeathers.FirstOrDefault(c => c.CityUniqueCode == id);
-            if (selectedCity == null)
-            {
-                return NotFound();
-            }
-            return View(selectedCity);
+            ViewBag.bgcolor = _bgcolor;
+            var selectedCity = _cityWeathers.FirstOrDefault(c => c.CityUniqueCode == id);
+            return selectedCity == null ? NotFound() : View(selectedCity);
         }
 
 
