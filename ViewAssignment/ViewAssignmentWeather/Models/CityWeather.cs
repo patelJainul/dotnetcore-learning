78054380f88ed6ac0c9021d221b036cf2743dc9b
Jainul Patel <jainul.h.patel@gmail.com>
2025-02-22 11:24:09 +0530

    Refactor CityWeather model to make TempretureFahrenheit non-nullable; update Index view to use ViewBag for background color and implement partial view for weather cards

--- ViewAssignment/ViewAssignmentWeather/Models/CityWeather.cs
+++ ViewAssignment/ViewAssignmentWeather/Models/CityWeather.cs
@@ -6,6 +6,6 @@ public class CityWeather
     public string? CityUniqueCode { get; set; }
     public string? CityName { get; set; }
     public DateTime DateAndTime { get; set; }
-    public int? TempretureFahrenheit { get; set; }
+    public int TempretureFahrenheit { get; set; }
 
 }
