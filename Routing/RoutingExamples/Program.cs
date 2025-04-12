317e974efe1d3b52fcc0bf92d1bac303c3763dea
Jainul Patel <jainul.h.patel@gmail.com>
2025-04-12 12:36:18 +0530

    Refactor endpoint mapping in Program.cs for improved readability and maintainability. Adjust formatting and comments for clarity.

--- Routing/RoutingExamples/Program.cs
+++ Routing/RoutingExamples/Program.cs
@@ -2,13 +2,16 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.UseRouting();
-app.UseEndpoints(endpoints => {
+app.UseEndpoints(endpoints =>
+{
     _ = endpoints.MapPost("static", async context => await context.Response.WriteAsync("test!"));
-    _ = endpoints.MapGet("file/{filename}.{ext:minlength(3)?}", async context => {
-        string? fileName = context.Request.RouteValues["filename"] as string;
-        string? ext = context.Request.RouteValues["ext"] as string;
-        await context.Response.WriteAsync($"Filename: {fileName}, Extension: {ext}");
-    });
+    _ = endpoints.MapGet(
+        "file/{filename}.{ext:minlength(3)?}",
+        async (string filename, string? ext, HttpContext context) =>
+            // string? fileName = context.Request.RouteValues["filename"] as string;
+            // string? ext = context.Request.RouteValues["ext"] as string;
+            await context.Response.WriteAsync($"Filename: {filename}, Extension: {ext}")
+    );
 });
 
 app.Run(async (context) => await context.Response.WriteAsync("Hello World!"));
