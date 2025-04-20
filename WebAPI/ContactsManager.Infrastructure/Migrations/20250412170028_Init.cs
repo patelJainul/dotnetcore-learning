c83ee3d283cd165fac717871cc5b93df8c03e8d7
Jainul Patel <jainul.h.patel@gmail.com>
2025-04-20 16:37:59 +0530

    Refactor migration code for improved readability by consolidating method calls and adjusting formatting.

--- WebAPI/ContactsManager.Infrastructure/Migrations/20250412170028_Init.cs
+++ WebAPI/ContactsManager.Infrastructure/Migrations/20250412170028_Init.cs
@@ -13,8 +13,7 @@ namespace ContactsManager.Infrastructure.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.EnsureSchema(
-                name: "dbo");
+            migrationBuilder.EnsureSchema(name: "dbo");
 
             migrationBuilder.CreateTable(
                 name: "Cities",
@@ -22,12 +21,13 @@ namespace ContactsManager.Infrastructure.Migrations
                 columns: table => new
                 {
                     CityId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Cities", x => x.CityId);
-                });
+                }
+            );
 
             migrationBuilder.InsertData(
                 schema: "dbo",
@@ -36,16 +36,15 @@ namespace ContactsManager.Infrastructure.Migrations
                 values: new object[,]
                 {
                     { new Guid("553b2e05-d568-43d3-bb5e-a20511dbb694"), "New York" },
-                    { new Guid("e1f6827d-4ad4-4e09-8ec3-50fe2e3396f6"), "Los Angeles" }
-                });
+                    { new Guid("e1f6827d-4ad4-4e09-8ec3-50fe2e3396f6"), "Los Angeles" },
+                }
+            );
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "Cities",
-                schema: "dbo");
+            migrationBuilder.DropTable(name: "Cities", schema: "dbo");
         }
     }
 }
